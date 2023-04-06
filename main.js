b9cb0978fd56ff952d925d12ddf54b7335e87466
Pacha <rodrigonzalez12@gmail.com>
2023-04-06 01:34:45 -0300

    setting up the stage fopr the project

---
+++ main.js
@@ -0,0 +1,12 @@
+const moves = document.getElementById('moves');
+const container = document.querySelector(".container");
+const startButton = document.getElementById('start-button');
+const coverScreen = document.querySelector(".cover-screen");
+const result = document.getElementById("result");
+let currentElement = "";
+let movesCount, imagesArr = [];
+const isTouchDevice = ()=> {
+    try {
+        
+    }
+}

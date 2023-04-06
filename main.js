4137768a60dc91e6abf46cad4ed69d12bc65e1ad
Pacha <rodrigonzalez12@gmail.com>
2023-04-06 20:44:51 -0300

    + adelantos en JS

--- main.js
+++ main.js
@@ -20,5 +20,27 @@ const randomNumber = () => Math.floor(Math.random()*8+1);
 const getCoords = (element) => {
     const [row, col] = element.getAttribute("data-position").split("_");
     return [parseInt(row),parseInt(col)];
-    
+};
+
+const checkAdjacent = (row1, row2, col1, col2) => {
+    if (row1 == row2) {
+        if (col1 == col2 || col2 == col1 + 1) {
+            return true;
+        }
+    } else if (col1 == col2) {
+        if (row2 == row1-1||row2 == row1+1) {
+            return true;
+        }
+    }
+    return false;
+};
+
+const randomImage = () => {
+    while(imagesArr.length < 8) {
+        let randomVal = randomNumber();
+        if (!imagesArr.includes(randomVal)){
+            imagesArr.push(randomVal);
+        }
+    }
+    imagesArr.push(9);
 }

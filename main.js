4a0374c1d641d420eb81a97f42f6e4235ad23a84
Rodrigo <rodrigonzalez12@gmail.com>
2023-04-08 21:05:20 -0300

    Juego funcional

--- main.js
+++ main.js
@@ -24,11 +24,11 @@ const getCoords = (element) => {
 
 const checkAdjacent = (row1, row2, col1, col2) => {
     if (row1 == row2) {
-        if (col1 == col2 || col2 == col1 + 1) {
+        if (col2 == col1 - 1 || col2 == col1 + 1) {
             return true;
         }
     } else if (col1 == col2) {
-        if (row2 == row1-1||row2 == row1+1) {
+        if (row2 == row1 - 1 || row2 == row1 + 1) {
             return true;
         }
     }
@@ -50,15 +50,55 @@ const gridGenerator = () => {
     for (let i=0;i<3;i++) {
         for (let j=0;j<3;j++) {
             let div = document.createElement('div');
-            div.setAttribute('data-position', `${i}_${i}`);
-            div.addEventListener('click', selecImage);
+            div.setAttribute('data-position', `${i}_${j}`);
+            div.addEventListener('click', selectImage);
             div.classList.add('image-container');
-            div.innerHTML = `<img src="image_part_00${imagesArr[count]}.png" class="image ${imageArr[count] == 9 ? "target" : "" } " data index="${imagesArr[count]}" />`;
-            count +=1;
-            container.appendChild(div)
+            div.innerHTML = `<img src="./image_part_00${imagesArr[count]}.png" class="image ${imagesArr[count] == 9 ? "target" : ""}" data-index="${imagesArr[count]}"/>`;
+            count += 1;
+            container.appendChild(div);
         }
     }
 };
+
+const selectImage = (e) => {
+    e.preventDefault();
+    currentElement = e.target;
+    let targetElement = document.querySelector('.target');
+    let currentParent = currentElement.parentElement;
+    let targetParent = targetElement.parentElement;
+
+    const [row1, col1]  = getCoords(currentParent);
+    const [row2, col2] = getCoords(targetParent);
+
+    if(checkAdjacent(row1, row2, col1, col2)) {
+        currentElement.remove();
+        targetElement.remove();
+
+        let currentIndex = parseInt(currentElement.getAttribute('data-index'));
+        let targetIndex = parseInt(targetElement.getAttribute('data-index'));
+
+        currentElement.setAttribute('data-index', targetIndex);
+        targetElement.setAttribute('data-index', currentIndex);
+
+        currentParent.appendChild(targetElement);
+        targetParent.appendChild(currentElement);
+
+        let currentArrIndex = imagesArr.indexOf(currentIndex);
+        let targetArrIndex = imagesArr.indexOf(targetIndex);
+        [imagesArr[currentArrIndex],imagesArr[targetArrIndex]] = [imagesArr[targetArrIndex] ,imagesArr[currentArrIndex]];
+
+        if(imagesArr.join('') == '123456789') {
+            setTimeout(() => {
+                coverScreen.classList.remove('hide');
+                container.classList.add('hide');
+                result.innerText = `Total Moves: ${movesCount}`;
+                startButton.innerText = 'Restart Game';
+            }, 1000)
+        }
+        movesCount += 1;
+        moves.innerText = `Moves: ${movesCount}`;
+    }
+}
 
 startButton.addEventListener('click', () => {
     container.classList.remove('hide');
@@ -68,6 +108,12 @@ startButton.addEventListener('click', () => {
     randomImages();
     gridGenerator();
     movesCount = 0;
-    moves.innerText = `Moves: ${movesCount}`
-})
+    moves.innerText = `Moves: ${movesCount}`;
+});
+
+window.onload = () => {
+    coverScreen.classList.remove('hide');
+    container.classList.add('hide');
+
+}
 
